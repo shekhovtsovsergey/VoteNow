b77363054ab314d4606ec9493ad1c42a37d6bde4
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-04-27 22:38:10 +0300

    Initial commit

---
+++ src/main/java/com/example/votenow/VoteNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.votenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteNowApplication.class, args);
+    }
+
+}
